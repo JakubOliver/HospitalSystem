841e586d99535226906047f7f12d3e332260c94f
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-02-07 19:20:03 +0100

    Creation of patient database and add operation
    New constructor for Database class, which validate whether
    the patients table exists and decides whether it is needed to
    create one.
    
    Also was created one method in Database class that add
    new patient into the database.

--- src/main/java/hospitalsystem/Main.java
+++ src/main/java/hospitalsystem/Main.java
@@ -1,27 +1,24 @@
 package hospitalsystem;
 
+import hospitalsystem.database.Database;
 import hospitalsystem.personnel.Doctor;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Main {
-    public static void connect() {
-        // connection string
-        var url = "jdbc:sqlite:database.db";
-
-        try (var conn = DriverManager.getConnection(url)) {
-            System.out.println("Connection to SQLite has been established.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     public static void main(String[] args) {
-        //connect();
-
         Doctor doctor = new Doctor(0, "John", "Brown", 1,3,1990, "Surgeon");
 
-        System.out.println(doctor);
+        //System.out.println(doctor);
+
+        Database database = new Database("jdbc:sqlite:database.db");
+
+        try {
+            System.out.println(database.addPatient("John", "Brown", LocalDate.now(), "Cancer"));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
---
+++ src/main/java/hospitalsystem/database/Database.java
@@ -0,0 +1,58 @@
+package hospitalsystem.database;
+
+import hospitalsystem.personnel.Patient;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class Database {
+    private static final String insertPatient = "INSERT INTO patients(firstname, lastname, birth_date, anamnesis) VALUES (?, ?, ?, ?)";
+
+    private final String url;
+
+    public Database(String url){
+        this.url = url;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to SQLite has been established.");
+
+            Statement stmt =  conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS patients (id INTEGER PRIMARY KEY, firstname text NOT NULL, lastname text NOT NULL, birth_date TEXT NOT NULL CHECK ( birth_date GLOB '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]' and date(birth_date) IS NOT NULL), anamnesis text NOT NULL);");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Patient addPatient(String firstname, String lastname, LocalDate birthDate, String anamnesis) throws Exception {
+        try (Connection conn = DriverManager.getConnection(url)) {
+            conn.setAutoCommit(false);
+            PreparedStatement stmt = conn.prepareStatement(insertPatient);
+
+            stmt.setString(1, firstname);
+            stmt.setString(2, lastname);
+            stmt.setString(3, birthDate.toString());
+            stmt.setString(4, anamnesis);
+
+            stmt.executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    System.out.println("Patient successfully added to the database");
+                    conn.commit();
+
+                    return new Patient(rs.getInt(1), firstname, lastname, birthDate, anamnesis);
+                }
+
+                throw new SQLException("Unable to get generated key.");
+            } catch (SQLException e) {
+                conn.rollback();
+
+                throw e;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+            throw new Exception("Unable to correctly add patient into database"); //TODO: use custom exception and use constant for the message
+        }
+    }
+}
