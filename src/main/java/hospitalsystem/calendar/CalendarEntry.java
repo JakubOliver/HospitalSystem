84a07f5bfe10e2eef7a6dd8845da3780abdbb3f7
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-02-09 13:05:25 +0100

    Basic structure for CalendarEntry.

--- src/main/java/hospitalsystem/calendar/CalendarEntry.java
+++ src/main/java/hospitalsystem/calendar/CalendarEntry.java
@@ -1,5 +1,30 @@
 package hospitalsystem.calendar;
 
-public class CalendarEntry {
+import hospitalsystem.personnel.Doctor;
+import hospitalsystem.personnel.Patient;
 
+import java.time.LocalDateTime;
+
+public class CalendarEntry {
+    private final int id;
+
+    private final int patientId;
+    private final int doctorId;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public CalendarEntry(int id, int patientId, int DoctorId,  LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+
+        this.patientId = patientId;
+        this.doctorId = DoctorId;
+
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public CalendarEntry(int id, Patient patient, Doctor doctor, LocalDateTime startTime, LocalDateTime endTime) {
+        this(id, patient.getId(), doctor.getId(), startTime, endTime);
+    }
 }
