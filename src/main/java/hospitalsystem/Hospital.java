73fd52d25269f394ae8b4672799b2a22604423f7
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-02-10 10:24:48 +0100

    Javadoc comments for Hospital

--- src/main/java/hospitalsystem/Hospital.java
+++ src/main/java/hospitalsystem/Hospital.java
@@ -1,36 +1,45 @@
 package hospitalsystem;
 
-import hospitalsystem.UI.MainMenu;
-import hospitalsystem.calendar.CalendarEntry;
 import hospitalsystem.calendar.util.CalendarEntryData;
 import hospitalsystem.database.Database;
 import hospitalsystem.database.DatabaseException;
-import hospitalsystem.personnel.Doctor;
 import hospitalsystem.personnel.Patient;
 import hospitalsystem.personnel.util.DoctorData;
 import hospitalsystem.personnel.util.PatientData;
-import hospitalsystem.util.HospitalAPI;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/**
+ * Hospital system for managing patients, doctors and appointments.
+ */
 public class Hospital {
     private final Database database;
 
+    /**
+     * Creates Hospital which content is based on the data in provided database.
+     *
+     * @param databasePath Path to the database.
+     */
     public Hospital(String databasePath){
         database = new Database(databasePath);
     }
 
+    /**
+     * Adds new patient into hospital system.
+     *
+     * @param patientData Patients data which describes new patient.
+     */
     public void addPatient(PatientData patientData) {
         //TODO: validate
 
         try {
-            database.addPatient(patientData.person().firstName(), patientData.person().lastName(), patientData.person().dateOfBirth(), patientData.details().anamnesis()); //TODO: rozbalovani az na urovni databaze
+            database.addPatient(
+                    patientData.person().firstName(),
+                    patientData.person().lastName(),
+                    patientData.person().dateOfBirth(),
+                    patientData.details().anamnesis()
+            ); //TODO: rozbalovani az na urovni databaze
 
             System.out.println("Success!");
         } catch (DatabaseException e){
@@ -38,6 +47,12 @@ public class Hospital {
         }
     }
 
+    /**
+     * Returns info about patient with provided id.
+     *
+     * @param id Id that identifies patient.
+     * @return info about patient.
+     */
     public String getPatientInfo(int id){
         try{
             return database.getPatient(id).toString();
@@ -46,6 +61,11 @@ public class Hospital {
         }
     }
 
+    /**
+     * Returns list of information about every patient in the system.
+     *
+     * @return list of information about every patient in the system.
+     */
     public List<String> findAllPatient(){
         try{
             List<Patient> patients = database.getAllPatients();
@@ -55,13 +75,23 @@ public class Hospital {
             System.out.println("Error: " + e.getMessage());
         }
 
-        return new ArrayList<String>();
+        return new ArrayList<>();
     }
 
+    /**
+     * Adds new doctor into the system.
+     *
+     * @param doctorData Doctor data that describes the new doctor.
+     */
     public void addDoctor(DoctorData doctorData) {
         //TODO: validate
         try {
-            database.addDoctor(doctorData.person().firstName(), doctorData.person().lastName(), doctorData.person().dateOfBirth(), doctorData.details().specialization());
+            database.addDoctor(
+                    doctorData.person().firstName(),
+                    doctorData.person().lastName(),
+                    doctorData.person().dateOfBirth(),
+                    doctorData.details().specialization()
+            );
 
             System.out.println("Success!");
         } catch (DatabaseException e){
@@ -69,6 +99,11 @@ public class Hospital {
         }
     }
 
+    /**
+     * Adds new appointment into the system.
+     *
+     * @param calendarEntryData Calendar Entry data that describes the new appointment.
+     */
     public void addAppointment(CalendarEntryData calendarEntryData) {
         //TODO: validate
 
