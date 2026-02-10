c58bb6fba6b8cd42bbcec01b7f3a6410727d8bdc
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-02-10 10:16:27 +0100

    Javadoc comments for main class.

--- src/main/java/hospitalsystem/Main.java
+++ src/main/java/hospitalsystem/Main.java
@@ -1,17 +1,19 @@
 package hospitalsystem;
 
 import hospitalsystem.UI.MainMenu;
-import hospitalsystem.database.Database;
-import hospitalsystem.personnel.Doctor;
 import hospitalsystem.util.HospitalAPI;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.time.LocalDate;
-
+/**
+ * Contains main entry point of the program.
+ */
 public class Main {
     private static final String databasePath = "jdbc:sqlite:database.db";
 
+    /**
+     * Main entry point of the program.
+     *
+     * @param args Arguments of the program.
+     */
     public static void main(String[] args) {
         Hospital hospital = new Hospital(databasePath);
 
