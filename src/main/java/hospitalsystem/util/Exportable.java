0d03b9d543e772a39298289245ced6de5273b8ad
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-02-17 21:52:30 +0100

    Adds javadoc comments for utils.

--- src/main/java/hospitalsystem/util/Exportable.java
+++ src/main/java/hospitalsystem/util/Exportable.java
@@ -1,5 +1,13 @@
 package hospitalsystem.util;
 
+/**
+ * Requires classes to implement export methods that could be use while exporting content of the classes into files.
+ */
 public interface Exportable {
+    /**
+     * Text format that will be used for exporting object.
+     *
+     * @return text that will be used for exporting object.
+     */
     String export();
 }
