df7d44110ca7e2850012721a971ba18a34ad33bd
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2026-07-25 18:35:50 +0200

    Extract common add personal logic to common ancestor.

---
+++ GUI/src/main/java/cz/cuni/kubinja/hospitalsystem/GUI/internal/AddPersonnelPage.java
@@ -0,0 +1,57 @@
+package cz.cuni.kubinja.hospitalsystem.GUI.internal;
+
+import cz.cuni.kubinja.hospitalsystem.core.Hospital;
+import cz.cuni.kubinja.hospitalsystem.core.packet.DataPacket;
+import cz.cuni.kubinja.hospitalsystem.core.personnel.Person;
+import javafx.beans.binding.BooleanExpression;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+/**
+ * Shared workflow for creating a patient or doctor.
+ *
+ * @param <T> Type of personnel being created.
+ */
+public abstract class AddPersonnelPage<T extends Person> extends PersonnelActionPage<T> {
+    protected AddPersonnelPage(
+            Navigator navigator,
+            Hospital hospital,
+            String personnelName
+    ) {
+        super(navigator, hospital, personnelName);
+    }
+
+    @Override
+    public final String getTitle() {
+        return "Add new " + personnelNameLowerCase();
+    }
+
+    @Override
+    protected final Node createBody() {
+        Node form = createPersonnelForm();
+        Button save = createActionButton(
+                "Save " + personnelNameLowerCase()
+        );
+        save.setDefaultButton(true);
+        save.disableProperty().bind(formValidProperty().not());
+        save.setOnAction(event -> savePersonnel());
+
+        return createCenteredBox(22, form, save);
+    }
+
+    protected abstract Node createPersonnelForm();
+
+    protected abstract BooleanExpression formValidProperty();
+
+    protected abstract DataPacket<T> addPersonnel();
+
+    private void savePersonnel() {
+        DataPacket<T> packet = addPersonnel();
+        if (!showApiError(packet)) {
+            complete(
+                    personnelName() + " was added with ID "
+                            + packet.data.getId() + "."
+            );
+        }
+    }
+}
